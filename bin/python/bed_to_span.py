e0778a017f7cbaf33fe71c285124f76ee450c3a9
jnoms <jasonlnomburg@gmail.com>
2021-03-17 21:07:09 -0400

    Major update. Can now take in an illumina span file and use that to split dRNAseq spans into supported and unsupported categories.

--- bin/python/bed_to_span.py
+++ bin/python/bed_to_span.py
@@ -74,6 +74,77 @@ def get_args():
         concerned about junctions.
         '''
     )
+    parser.add_argument(
+        '-i',
+        '--illumina_spans',
+        type=str,
+        required=False,
+        default="",
+        help='''
+        Path to the spans file, created by this script, from illumina reads. 
+        If this is specified, this script will consider junctions in the input BED
+        by illumia confidence.
+        '''
+    )
+    parser.add_argument(
+        '-g',
+        '--n_genomes',
+        type=int,
+        required=False,
+        default=20,
+        help='''
+        Only required if incorporating illumina junctions support.
+
+        This should specify the number of genomes the input bed file was mapped
+        against. Default is set to 20, so as long as the bed is from a reference
+        that is 20 genomes or less you are all set.
+        '''
+    )
+    parser.add_argument(
+        '-u',
+        '--n_junctions',
+        type=int,
+        required=False,
+        default=5,
+        help='''
+        Only required if incorporating illumina junctions support.
+        Default: 5
+
+        This is an interger value detailing the minimum number of 
+        illumina reads with all junctions within a transcript of
+        the input BED to designate the BED as high-confidence. 
+        '''
+    )
+    parser.add_argument(
+        '-l',
+        '--genome_length',
+        type=int,
+        required=False,
+        default=0,
+        help='''
+        Only required if incorporating illumina junctions support.
+
+        This is the genome length of the viral reference genome. Used to propogate
+        illumina junctions forward to n_genomes.
+
+        SV40: 5243
+        BK: 5153
+        '''
+    )
+    parser.add_argument(
+        '-x',
+        '--output_spans_unsupported',
+        type=str,
+        required=False,
+        default="",
+        help='''
+        Only required if incorporating illumina junctions support.
+
+        Path to the output file containing the spans from the unsupported transcripts.
+        These transcripts contain at least one junction with fewer illumina reads than
+        n_junctions supporting them.
+        '''
+    )
 
     args = parser.parse_args()
 
@@ -89,6 +160,8 @@ def get_args():
 #------------------------------------------------------------------------------#
 bed_cols = "chrom tx_start tx_end name score strand thickStart thickEnd itemRgb blockCount blockSizes blockStarts"
 bed_cols = bed_cols.split(" ")
+span_columns = "name start end strand span_type tx_start tx_end tx_class tx_class_count tx_class_illumina_support".split(" ")
+
 
 #------------------------------------------------------------------------------#
 # Transcript object and associated functions
@@ -120,18 +193,20 @@ def find_spans(tx_start, tx_end, blockSizes, blockStarts, tx_name):
     - blockStarts: blockStarts field from the bed file. May end with a free comma.
     - tx_name: Name of the transcript. This is just used for the error messages.
 
-    blockSizes and blockStarts should be the same length. These items describe each EXON, e.g.
-    each exon starts at position mentioned in index i of blockStarts and has a lenght defined at
-    index i of blockSizes.
+    blockSizes and blockStarts should be the same length. These items describe
+    each EXON, e.g. each exon starts at position mentioned in index i of
+    blockStarts and has a lenght defined at index i of blockSizes.
 
     Output: tuple (spans, juncs)
-    - spans: List of tuples that are alternating between exons and introns. This object shows the coordinates
+    - spans: List of tuples that are alternating between exons and introns. This
+      object shows the coordinates
              for each span. Ex:
              [(56, 194, 'exon'),
              (194, 334, 'intron'),
              (334, 426, 'exon')]
 
-    - juncs: list of tuples that simply has the 5' and 3' position of each junction - e.g. only the coordinates
+    - juncs: list of tuples that simply has the 5' and 3' position of each
+      junction - e.g. only the coordinates
              from the intron spans. Ex:
              [(194, 334)]
     """
@@ -240,6 +315,139 @@ def parse_bed_to_tx_objects(bed_path, junc_reads_only="no"):
 
     return tx_objects
 
+def illumina_spans_to_junction_counts(illumina_spans, n_genomes, genome_length, span_columns):
+    """
+    This function reads in and counts the junctions from illumian spans. It first "slides"
+    all junctions/introns so they start in the first genome. It then counts instances of
+    each junction. Finally, it copies the same count for each position in all other subsequent
+    n_genomes (e.g. start + genome_length*i for i in range(n_genomes) and same for end)
+    
+    Input:
+        - illumina_spans: path to the illumian spans file
+        - n_genomes: Number of genomes to propogate forward
+        - genome_length: Length of a single viral reference.
+        - span_columns: colnames of the span file.
+        
+    Output:
+        - propogated_junction_counts: Counter of structure
+        (start, end, strand): count
+        
+    """
+    
+    # If no illumina_spans provided, just return an empty counter
+    if illumina_spans == "":
+        return Counter()
+
+    # (start, end, strand):count
+    junction_counts = Counter()
+
+    with open(illumina_spans) as infile:
+
+        for line in infile:
+
+            if line.startswith("name"): continue
+
+            line = line.split()
+
+            # Keep only introns
+            if line[span_columns.index("span_type")] == "exon":
+                continue
+
+            # Count start, stop, and strand
+            start = int(line[span_columns.index("start")])
+            end = int(line[span_columns.index("end")])
+            strand = line[span_columns.index("strand")]
+
+            # Slide junctions to consolidate in the first genome - will propogate 
+            # forward in the next loop
+            while start > genome_length:
+                start = start - genome_length
+                end = end - genome_length
+
+            # Add to count
+            junction_counts[(start, end, strand)] += 1
+
+    # Propogate to n genomes
+    propogated_junction_counts = Counter()
+    for (start, end, strand), count in junction_counts.items():
+
+        for i in range(n_genomes):
+            new_start = start + (genome_length * i)
+            new_end = end + (genome_length * i)
+
+            propogated_junction_counts[(new_start, new_end, strand)] = count
+
+    return propogated_junction_counts
+
+
+def split_tx_by_support(tx_objects, illumina_junction_counts, n_junctions):
+    """
+    This function takes in the tx_objects dictionary and separates transcripts
+    that are supported by > n_junctions illumina junctions and those that are
+    not. It returns two dictionary containing the reads in question.
+    
+    If no illumina spans were provided, illumina_junction_counts will be an
+    empty Counter(). All tx_objects will then immedaitely be added to the
+    supported_tx_objects output dictionary.
+    
+    Input:
+        - tx_objects: Dict (tx_name:tx) for each transcript. tx is a transcript
+        object with all of the transcript information.
+        - illumina_junctions_counts: Counter() of structure (start, end, strand):count
+        that details number of supporting illumina reads for each junction.
+        - n_junctions: The minimum number of illumina reads to support ALL JUNCTIONS
+        in a tx for the tx to be considered supported. AGAIN, if a single junction
+        within a multi-junction tx is not-supported, the entire transcript is labled 
+        not-supported.
+        
+    Output:
+        - tuple of (supported_tx_objects, unsupported_tx_objects)
+        - Each is a dictionary, containing supported vs unsupported transcripts.
+        - If the input illumina_junction_counts is an empty counter, all transcripts
+        will automatically be in supported_tx_objects
+        
+    """
+
+    supported_tx_objects = dict()
+    unsupported_tx_objects = dict()
+
+    for tx_name, tx in tx_objects.items():
+
+        # If there are no illumina counts, spand weren't provied. Just label
+        # the tx accordingly and put them as supported.
+        if illumina_junction_counts == Counter():
+            tx.illumina_support = "NA"
+            supported_tx_objects[tx_name] = tx
+            continue
+
+        # Determine if any junctions don't have > n illumina reads
+        illumina_support = "supported"
+        for junction in tx.juncs:
+            lookup = (junction[0], junction[1], tx.strand)
+
+            illumina_count = illumina_junction_counts[lookup]
+
+            if illumina_count < n_junctions:
+                illumina_support = "unsupported"
+                break
+
+        # Label tx will it's status
+        tx.illumina_support = illumina_support
+
+        # Split to new dictionary accordingly
+        if illumina_support == "supported":
+            supported_tx_objects[tx_name] = tx
+        else:
+            unsupported_tx_objects[tx_name] = tx
+
+
+    print("There are {} supported and {} unsupported transcripts.".format(
+        len(supported_tx_objects), len(unsupported_tx_objects))
+         )
+
+    return supported_tx_objects, unsupported_tx_objects
+
+
 def count_and_rank_juncs(tx_objects):
     """
     Takes in a dictionary of tx_objects, and returns a dictionary of structure
@@ -289,14 +497,14 @@ def load_rank_and_count(tx_objects, junction_ranks):
 
     return tx_objects
 
-def write_tx_spans(in_tx_dict, outfile_path):
+def write_tx_spans(in_tx_dict, outfile_path, span_columns):
 
     # Make output directory if needed
     out_dir = os.path.dirname(outfile_path)
     pathlib.Path(out_dir).mkdir(parents=True, exist_ok=True)
 
     # Initiate with colheaders
-    output = "name start end strand span_type tx_start tx_end tx_class tx_class_count\n".replace(" ", "\t")
+    output = "\t".join(span_columns) + "\n"
 
     with open(outfile_path, "w") as outfile_handle:
 
@@ -307,10 +515,10 @@ def write_tx_spans(in_tx_dict, outfile_path):
 
                 start, end, span_type = span
 
-                entry = "{} {} {} {} {} {} {} {} {}\n".format(
+                entry = "{} {} {} {} {} {} {} {} {} {}\n".format(
                     tx.name, start, end, tx.strand, span_type,
                     tx.tx_start, tx.tx_end, tx.tx_class,
-                    tx.tx_class_count
+                    tx.tx_class_count, tx.illumina_support
                 ).replace(" ", "\t")
 
                 output += entry
@@ -326,6 +534,11 @@ def main():
     bed_path = args.bed_path
     output_spans = args.output_spans
     junc_reads_only = args.junc_reads_only
+    illumina_spans = args.illumina_spans
+    n_genomes = args.n_genomes
+    n_junctions = args.n_junctions
+    genome_length = args.genome_length
+    output_spans_unsupported = args.output_spans_unsupported
 
     # Main
     #--------------------------------------------------------------------#
@@ -334,15 +547,26 @@ def main():
     # Parse the bed file
     tx_objects = parse_bed_to_tx_objects(bed_path, junc_reads_only)
 
+    # Extract junction counts from the illumian spans file
+    illumina_junction_counts = illumina_spans_to_junction_counts(illumina_spans, n_genomes, genome_length, span_columns)
+
+    # Split tx_objects into supported/not-support transcripts. If no illumina_spans were provided,
+    # just put all into the "supported" one.
+    supported_tx_objects, unsupported_tx_objects = split_tx_by_support(tx_objects, illumina_junction_counts, n_junctions)
+
     # Count and rank the junctions... make new dict of structure
     # junc: (rank, count)
-    junction_ranks = count_and_rank_juncs(tx_objects)
+    supported_junction_ranks = count_and_rank_juncs(supported_tx_objects)
+    unsupported_junction_ranks = count_and_rank_juncs(unsupported_tx_objects)
 
     # Load rank (I'll call it tx_class) and count (tx_class_count) to each tx
-    tx_objects = load_rank_and_count(tx_objects, junction_ranks)
+    supported_tx_objects = load_rank_and_count(supported_tx_objects, supported_junction_ranks)
+    unsupported_tx_objects = load_rank_and_count(unsupported_tx_objects, unsupported_junction_ranks)
 
     # Write to output file.
-    write_tx_spans(tx_objects, output_spans)
+    write_tx_spans(supported_tx_objects, output_spans, span_columns)
+    if output_spans_unsupported != "":
+        write_tx_spans(unsupported_tx_objects, output_spans_unsupported, span_columns)
 
     print("{}: Finished".format(sys.argv[0]))
 
